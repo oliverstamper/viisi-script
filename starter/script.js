22f84e842cd99ca1a12f5609a424640122383a6d
ollystamper <148012712+oliverstamper@users.noreply.github.com>
2023-11-28 16:12:58 +0000

    Completed script

--- starter/script.js
+++ starter/script.js
@@ -90,33 +90,72 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var length = parseInt(prompt("Enter the desired length of your password. Must be between 8 and 128 characters"));
 
-  parseInt(prompt("Enter the desired length of your password. Must be between 8 and 128 characters"));
-
-  if (NaN(length)/length<8/length>128) {
-    "Please enter an appropriate password between 8 and 128 characters"
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter an appropriate password length between 8 and 128 characters");
+    return;
   }
-  var hasspecialCharacters = confirm("Do you wish to use special characters?")
-  var hasnumericCharacters = confirm("Do you wish to have numeric characters?")
-  var hasupperCasedCharacters = confirm("Do you wish to have upper case characters?")
-  var haslowerCasedCharacters = confirm("Do you wish to have lower case characters?")
 
+  let hasSpecialCharacters = confirm("Do you wish to use special characters?");
+  let hasNumericCharacters = confirm("Do you wish to have numeric characters?");
+  let hasUpperCasedCharacters = confirm("Do you wish to have upper case characters?");
+  let hasLowerCasedCharacters = confirm("Do you wish to have lower case characters?");
+
+  if (!hasLowerCasedCharacters && !hasUpperCasedCharacters && !hasNumericCharacters && !hasSpecialCharacters) {
+    alert("Please select at least one character type.");
+    return;
+  }
+
+  return {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters
+  };
 }
 
-// Function for getting a random element from an array
+// Function for getting random element from array
+
 function getRandom(arr) {
-
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
 
-// Function to generate password with user input
-function generatePassword() {
+// Function to generate password
 
+function generatePassword() {
+  var options = getPasswordOptions();
+  var charset = [];
+
+  if (options.hasLowerCasedCharacters) {
+    charset = charset.concat(lowerCasedCharacters);
+  }
+  if (options.hasUpperCasedCharacters) {
+    charset = charset.concat(upperCasedCharacters);
+  }
+  if (options.hasNumericCharacters) {
+    charset = charset.concat(numericCharacters);
+  }
+  if (options.hasSpecialCharacters) {
+    charset = charset.concat(specialCharacters);
+  }
+
+  var password = "";
+  for (var i = 0; i < options.length; i++) {
+    password += getRandom(charset);
+  }
+
+  alert("Generated Password: " + password);
 }
 
 // Get references to the #generate element
+
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
@@ -125,4 +164,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+
+generateBtn.addEventListener('click', generatePassword);
